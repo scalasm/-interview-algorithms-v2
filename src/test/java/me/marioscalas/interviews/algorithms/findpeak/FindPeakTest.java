82f7ee7a66c1daa042bae4e48ed7ebb258561c46
Mario Scalas <mario.scalas@linksmt.it>
2021-07-08 15:28:48 +0200

    Implement tests for findPeak1D() using @ParameterizedTest.

--- src/test/java/me/marioscalas/interviews/algorithms/findpeak/FindPeakTest.java
+++ src/test/java/me/marioscalas/interviews/algorithms/findpeak/FindPeakTest.java
@@ -8,20 +8,32 @@
  *******************************************************************************/
 package me.marioscalas.interviews.algorithms.findpeak;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class FindPeakTest {
 
-    @Test
-    void findPeak1D() {
-        assertThat(FindPeak.findPeak1D(new int[] {1,2,3,0})).isEqualTo(2);
-        assertThat(FindPeak.findPeak1D(new int[] {1,2})).isEqualTo(1);
-        assertThat(FindPeak.findPeak1D(new int[] {2,1})).isEqualTo(0);
-        assertThat(FindPeak.findPeak1D(new int[] {1,2,3,4})).isEqualTo(3);
-        assertThat(FindPeak.findPeak1D(new int[] {4,3,2,1})).isEqualTo(0);
-        assertThat(FindPeak.findPeak1D(new int[] {1})).isEqualTo(0);
-        assertThat(FindPeak.findPeak1D(new int[] {})).isEqualTo(-1);
+    @ParameterizedTest
+    @MethodSource("provideForFindPeak1D")
+    void findPeak1D(final int[] array, final int expectedPeakPosition) {
+        assertThat(FindPeak.findPeak1D(array)).isEqualTo(expectedPeakPosition);
     }
+
+    private static Stream<Arguments> provideForFindPeak1D() {
+        return Stream.of(
+                Arguments.of(new int[] {1,2,3,0}, 2),
+                Arguments.of(new int[] {1,2}, 1),
+                Arguments.of(new int[] {2,1}, 0),
+                Arguments.of(new int[] {1,2,3,4}, 3),
+                Arguments.of(new int[] {4,3,2,1}, 0),
+                Arguments.of(new int[] {1}, 0),
+                Arguments.of(new int[] {}, -1)
+        );
+    }
+
 }
